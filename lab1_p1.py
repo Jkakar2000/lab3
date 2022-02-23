39b2543ac34859b8daa387bfe3cd27c6cf01a494
006275447 <006275447@csusb.edu@jb358-28.cse.csusb.edu>
2022-02-23 10:58:10 -0800

    new files

---
+++ lab1_p1.py
@@ -0,0 +1,9 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+
+
+userName = input("Please enter your name: ")            #Takes the user name
+userAge = int(input("Please enter your age: "))         #Takes the user age
+birth = str(2021 - userAge)                             #Calculates the users age by subtracting the current year by their age
+print(userName + " was born in the year " + birth)      #outputs user name along with their birth year
+
